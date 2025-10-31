73fb59aafda5c8dfc5bd06bc285b12903688b63b
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-11-01 04:27:14 +0600

    implement doctor get, update apis with adding specialty

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,41 @@
+import type { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import pick from "../../helper/pick";
+import sendResponse from "../../shared/sendResponse";
+import { DoctorService } from "./doctor.service";
+import type { IOptions } from "../../helper/paginationHelper";
+import { doctorFilterableFields } from "./doctor.constant";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const filters = pick(req.query, doctorFilterableFields);
+
+  const result = await DoctorService.getAllFromDB(filters, options as IOptions);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor fetched successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+
+  const {id} = req.params;
+
+  const result = await DoctorService.updateIntoDB(id, req.body);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor updated successfully!",
+    data: result,
+  });
+});
+
+export const DoctorController = {
+  getAllFromDB,
+  updateIntoDB,
+};
