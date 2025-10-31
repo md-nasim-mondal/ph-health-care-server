619cbac209cba014acbbdaedc9f004f8ccb7c47e
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-31 22:59:43 +0600

    modify error using prisma errro

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,39 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    let success = false;
-    let message = err.message || "Something went wrong!";
-    let error = err;
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      (message = "Duplicate key error!"), (error = err.meta);
+    }
 
-    res.status(statusCode).json({
-        success,
-        message,
-        error
-    })
+    if (err.code === "P1000") {
+      message = "Authentication failed against database server";
+      error = err?.meta;
+    }
+
+    if (err.code === "P2003") {
+      message = "Foreign key constraint failed!";
+      error = err?.meta;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
 };
 
 export default globalErrorHandler;
