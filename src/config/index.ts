ab1c814500060585be812c6dac4ea7072156b16d
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-11-02 17:52:55 +0600

    make some auth api like get me, refresh-token, change-password, fogot-password, reset-password

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,5 +14,21 @@ export default {
   },
   openRouterApiKey: process.env.OPEN_ROUTER_API_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET
+  stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+  emailSender: {
+    email: process.env.EMAIL_SENDER_EMAIL,
+    app_pass: process.env.EMAIL_SENDER_APP_PASS,
+  },
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    access_token_expires_in: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_SECRET,
+    reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+  },
+  reset_pass_link: process.env.RESET_PASS_LINK,
+  bcrypt: {
+    salt_round: process.env.SALT_ROUND,
+  },
 };
